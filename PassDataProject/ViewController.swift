f197d44b51ea1084187254edcb88d98166a0e473
VNavrockij <vitalik.nav@gmail.com>
2022-04-21 18:00:59 +0300

    add: basic UI

--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBAction func sendPressed(sender: UIButton) {
+        
     }
 
 
